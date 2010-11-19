a99ad6500a95f4e48adcc588d1c0b58854a16349
arthurdesribeiro <arthurdesribeiro@c27e9e63-0d3b-bda6-c4b6-c3d10fa10cbe>
2010-11-19 03:42:00 +0000

    Classe para manipular arquivos atuando de forma independente...

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -7,36 +7,17 @@ import java.util.Scanner;
 
 public class Main {
 
-	HashMap<String, String[]> dados = new HashMap<String, String[]>();
-	
-	public void pegarDados() {
-		try {
-	        BufferedReader in = new BufferedReader(new FileReader("roteador.config"));
-	        String str;
-	        while(in.ready()) {
-	        	str = in.readLine();
-	        	String [] valor = new String[2];
-		        String [] splitStr = str.split(" ");
-		        valor[0] = splitStr[1];
-		        valor[1] = splitStr[2];
-		        this.dados.put(splitStr[0], valor);
-	        }
-		} catch (IOException e) {
-			System.out.println("Arquivo não encontrado!");
-		}
-	}
-	
 	public Node criarNo(int id) {
 		Node no = null;
 	    boolean valido = false;
-	    for(String chave : this.dados.keySet()) {
+	    for(String chave : FileManager.getDados().keySet()) {
 		    int identificador = Integer.parseInt(chave);
 		    if (identificador != id) {
 		    	continue;
 		    }
 		    valido = true;
-		    int porta = Integer.parseInt(dados.get(chave)[0]);
-		    String ip = dados.get(chave)[1];
+		    int porta = Integer.parseInt(FileManager.getDados().get(chave)[0]);
+		    String ip = FileManager.getDados().get(chave)[1];
 		    try {
 		    	no = new Node(id, porta, ip);		        	
 		    	System.out.println("Criou socket na porta: " + porta);
@@ -58,10 +39,10 @@ public class Main {
 	        	str = in.readLine();
 		        String [] splitStr = str.split(" ");
 		        if(Integer.parseInt(splitStr[0]) == no.getId()) {
-		        	NodeDescriptor desc = new NodeDescriptor(Integer.parseInt(splitStr[1]), dados.get(splitStr[1])[1], Integer.parseInt(dados.get(splitStr[1])[0]));
+		        	NodeDescriptor desc = new NodeDescriptor(Integer.parseInt(splitStr[1]), FileManager.getDados().get(splitStr[1])[1], Integer.parseInt(FileManager.getDados().get(splitStr[1])[0]));
 		        	no.addNeighbor(desc, Integer.parseInt(splitStr[2]));
 		        } else if(Integer.parseInt(splitStr[1]) == no.getId()) {
-		        	NodeDescriptor desc = new NodeDescriptor(Integer.parseInt(splitStr[0]), dados.get(splitStr[0])[1], Integer.parseInt(dados.get(splitStr[0])[0]));
+		        	NodeDescriptor desc = new NodeDescriptor(Integer.parseInt(splitStr[0]), FileManager.getDados().get(splitStr[0])[1], Integer.parseInt(FileManager.getDados().get(splitStr[0])[0]));
 		        	no.addNeighbor(desc, Integer.parseInt(splitStr[2]));
 		        }
 	        }
@@ -72,7 +53,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Main m = new Main();
-		m.pegarDados();
+		FileManager.buildMap();
 		int id;
 		if (args.length > 1) {
 			id = Integer.parseInt(args[1]);
