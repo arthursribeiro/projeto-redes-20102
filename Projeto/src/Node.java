0d78f7c416f6cead7865b88a66901bdd4c612d50
dsurviver <dsurviver@c27e9e63-0d3b-bda6-c4b6-c3d10fa10cbe>
2010-11-19 03:17:28 +0000

    Many changes

--- Projeto/src/Node.java
+++ Projeto/src/Node.java
@@ -3,6 +3,9 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.Vector;
 
 public class Node {
@@ -13,6 +16,7 @@ public class Node {
 	private DistanceVector distanceVector;
 	private Vector<NodeDescriptor> neighbors;
 	private Server server;
+	private HashMap<Integer, Integer> distances;
 
 	public Node(int id, int port, String ip) throws SocketException {
 		this.setId(id);
@@ -21,17 +25,68 @@ public class Node {
 		this.distanceVector = new DistanceVector();
 		this.neighbors = new Vector<NodeDescriptor>();
 		this.server = new Server(port, this);
+		this.distances = new HashMap<Integer, Integer>();
+	}
+
+	public void removeNeighbor(NodeDescriptor node) {
+		this.neighbors.remove(node);
+		this.distanceVector.removeById(node.getId());
 	}
 	
+	public void pingNeighbors() {
+		Vector<NodeDescriptor> toRemove = new Vector<NodeDescriptor>();
+		for (NodeDescriptor node : this.neighbors) {
+			String ip = node.getIp();
+			int port = node.getPort();
+			try {
+				InetAddress ipAddress = InetAddress.getByName(ip);
+				byte ping[] = ("ping," + node.getId()).getBytes();
+				DatagramSocket wait = new DatagramSocket();
+				DatagramPacket pingPacket = new DatagramPacket(ping,
+						ping.length, ipAddress, port);
+				System.out.println(">>>>>>>>>> ping? " + node.getId());
+				wait.send(pingPacket);
+				byte pong[] = new byte[12];
+				DatagramPacket pongPacket = new DatagramPacket(pong,
+						pong.length, ipAddress, port);
+				wait.setSoTimeout(1000);
+				try {
+					wait.receive(pongPacket);
+				} catch (SocketTimeoutException e) {
+					System.out.println("Timeout! Pong not received!");
+					wait.close();
+					toRemove.add(node);
+					continue;
+				}
+				String responseId = (new String(pongPacket.getData()).trim())
+						.split(",")[1];
+				System.out.println("<<<<<<<<<< Pong! " + responseId);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (SocketException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		for (NodeDescriptor node : toRemove) {
+			this.removeNeighbor(node);
+		}
+	}
+
 	public void notifyVector() {
+		this.pingNeighbors();
 		for (NodeDescriptor node : this.neighbors) {
 			String ip = node.getIp();
 			int port = node.getPort();
 			try {
 				DatagramSocket socket = new DatagramSocket();
 				InetAddress ipAddress = InetAddress.getByName(ip);
-				byte vector[] = (id + "," + this.distanceVector.toString()).getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(vector, vector.length, ipAddress, port);
+				byte vector[] = (id + "," + this.distanceVector.toString())
+						.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(vector,
+						vector.length, ipAddress, port);
 				socket.send(sendPacket);
 			} catch (SocketException e) {
 				e.printStackTrace();
@@ -46,7 +101,7 @@ public class Node {
 	}
 
 	public void addNeighbor(NodeDescriptor node, int distance) {
-		System.out.println(node.getId() + " " + distance);
+		this.distances.put(node.getId(), distance);
 		if (!this.neighbors.contains(node)) {
 			this.neighbors.add(node);
 			this.distanceVector.append(new VectorPair(node.getId(), distance));
@@ -58,8 +113,8 @@ public class Node {
 	}
 
 	public void updateVector(DistanceVector newVector, int sourceId) {
-		VectorPair pair = distanceVector.getPairById(sourceId);
-		if (distanceVector.merge(newVector, pair.getDistance(), this)) {
+		if (distanceVector.merge(newVector, sourceId, distances.get(sourceId), this)) {
+			System.out.println("New distance vector: " + distanceVector);
 			this.notifyVector();
 		}
 	}
@@ -87,7 +142,7 @@ public class Node {
 	public String getIp() {
 		return ip;
 	}
-	
+
 	public String toString() {
 		return "Node: (" + id + ", " + ip + ", " + port + ")";
 	}
